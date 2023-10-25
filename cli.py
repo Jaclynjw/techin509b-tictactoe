68e0b73bf2b9a778638170b3afe965537dd3fbd7
Jaclynjw <chenjw18@uw.edu>
2023-10-25 15:36:47 -0700

    modified:   cli.py

--- cli.py
+++ cli.py
@@ -11,7 +11,26 @@ if __name__ == '__main__':
     while winner == None:
         print("TODO: take a turn!")
         # TODO: Show the board to the user.
+        print(board)
         # TODO: Input a move from the player.
+        
         # TODO: Update the board.
+
+        #check rows
+        for row in board:
+            if row[0]==row[1]==row[2]:
+                output = row[0] + ' won'
+            else:
+                output = 'Draw'
+        #check columns
+        for i in range(3):
+            if board[0][i]==board[1][i]==board[2][i]:
+                output = board[0][i] + ' won'
+            #check diagonals
+            if board[0][0]==board[1][1]==board[2][2] or board[2][0]==board[1][1]==board[2][0]:
+                output = board[1][1] + ' won'
+
+        print(output)     #print the output
+
         # TODO: Update who's turn it is.
         winner = 'X'  # FIXME
