0ce4b4cdedb12ba381874dafd11536d22f2b01d0
Jaclynjw <chenjw18@uw.edu>
2023-11-11 21:30:55 -0800

    modified:   __pycache__/logic.cpython-38.pyc 	modified:   cli.py 	modified:   logic.py
    Additional features added:
    
    The game should be playable by a single player or 2 players.
    In the case of a single player, the player should play against a 'bot'. For implementing this bot, you can just choose feasible random numbers as the inputs to the game from the bot (you can build more sophisticated logic for the inputs from the bots if you are interested).
    In the case of two players, the players should take turns entering the inputs in the CLI.
    The functionality should be implemented through objected-oriented dependency injection.
    The above steps are repeated until a winner emerges.

--- cli.py
+++ cli.py
@@ -2,25 +2,51 @@
 # the Tic-Tac-Toe game. This is where input and output happens.
 # For core game logic, see logic.py.
 
-from logic import make_empty_board, get_winner, other_player
+#from logic import make_empty_board, get_winner, other_player
+from logic import *
 
 
 # Reminder to check all the tests
 
 if __name__ == '__main__':
-    board = make_empty_board()
-    winner = None
-    sign = 'X'
-    while winner == None:
-        print("TODO: take a turn!")
-        # TODO: Show the board to the user.
-        print(board)
-        # TODO: Input a move from the player.
-        pos_x, pos_y = input("Enter a position(x,y), split with space: ").split()
-        # TODO: Update the board.
-        board[int(pos_x)][int(pos_y)] = sign
-        winner = get_winner(board)
-        # TODO: Update who's turn it is.
-        if not winner:
-            sign = other_player(sign)
-    print(winner)
+    game = TicTacTocGame()          #board = empty_board, winner = None, sign = 'X'
+
+    game_mode = input("Choose game mode: \n 1. Single Player \n 2. Two Players \n")
+    if game_mode == "1":
+        bot = BotPlayer()
+        player = input("Choose your sign: X or O (X goes first)\n")
+        game.print_board()
+        if player == "X":
+            while game.get_winner() == None:
+                print('Take your turn(X)')
+                
+                pos_x, pos_y = input("Enter a position(x,y), split with space: ").split()
+                game.board[int(pos_x)][int(pos_y)] = player
+                game.print_board()
+                #print(game.get_winner())
+                if game.get_winner() == None:
+                    pos_x, pos_y = bot.make_move(game.board)
+                    game.board[pos_x][pos_y] = "O"
+                    print('Bot\'s turn(O)')
+                    game.print_board()
+        elif player == "O":
+            while game.get_winner() == None:
+                pos_x, pos_y = bot.make_move(game.board)
+                game.board[pos_x][pos_y] = "X"
+                print('Bot\'s turn(X)')
+                game.print_board()
+                if game.get_winner() == None:
+                    print('Take your turn(O)')
+                    pos_x, pos_y = input("Enter a position(x,y), split with space: ").split()
+                    game.board[int(pos_x)][int(pos_y)] = player
+                    game.print_board()
+    if game_mode == "2":
+        while game.get_winner() == None:
+            game.print_board()
+            print(f"take a turn!({game.player})")
+            pos_x, pos_y = input("Enter a position(x,y), split with space: ").split()
+            game.board[int(pos_x)][int(pos_y)] = game.player
+            if game.get_winner() == None:
+                game.other_player()
+        game.print_board()
+    print(f"{game.get_winner()} wins!")
--- logic.py
+++ logic.py
@@ -2,37 +2,79 @@
 # or output happens here. The logic in this file
 # should be unit-testable.
 
-
-def make_empty_board():
-    return [
-        [None, None, None],
-        [None, None, None],
-        [None, None, None],
-    ]
+class TicTacTocGame():
+    def __init__(self):
+        self.board = self.make_empty_board()
+        self.player = "X"
+        self.winner = None
 
 
-def get_winner(board):
-    """Determines the winner of the given board.
-    Returns 'X', 'O', or None."""
-     #check rows
-    for row in board:
-        if row[0]==row[1]==row[2]:
-            output = row[0]
-        else:
-            output = None
-    #check columns
-    for i in range(3):
-        if board[0][i]==board[1][i]==board[2][i]:
-            output = board[0][i]
-        #check diagonals
-        if board[0][0]==board[1][1]==board[2][2] or board[2][0]==board[1][1]==board[2][0]:
-            output = board[1][1]
-    return output  # FIXME
+    def make_empty_board(self):
+        return [
+            [None, None, None],
+            [None, None, None],
+            [None, None, None],
+        ]
 
 
-def other_player(player):
-    """Given the character for a player, returns the other player."""
-    if player == "X":
-        return "O"
-    elif player == "O":
-        return "X"
+    def get_winner(self):
+        """Determines the winner of the given board.
+        Returns 'X', 'O', or None."""
+        #check rows
+        for row in self.board:
+            if row[0]==row[1]==row[2]:
+                output = row[0]
+            else:
+                output = None
+        #check columns
+        for i in range(3):
+            if self.board[0][i]==self.board[1][i]==self.board[2][i]:
+                output = self.board[0][i]
+            #check diagonals
+            if self.board[0][0]==self.board[1][1]==self.board[2][2] or self.board[2][0]==self.board[1][1]==self.board[2][0]:
+                output = self.board[1][1]
+        return output  # FIXME
+
+
+    def other_player(self):
+        """Given the character for a player, returns the other player."""
+        if self.player == "X":
+            self.player = "O"
+            #return "O"
+        elif self.player == "O":
+            self.player = "X"
+            #return "X"
+        
+        '''
+    def print_board(self):
+        """Prints the board."""
+        for row in self.board:
+            print(row)
+        #print('\n')
+        '''
+
+    def print_board(self):
+        max_width = max(len(str(cell)) for row in self.board for cell in row)
+
+        for row in self.board:
+            for cell in row:
+                cell_str = str(cell) if cell is not None else " "
+                print(cell_str.ljust(max_width), end=" | ")
+            print("\n" + "- " * max_width * 3)
+        
+
+class BotPlayer():
+    def __init__(self):
+        pass
+
+    def make_move(self, board):
+        """Given a board, returns a move."""
+        #find empty space (x,y)
+        empty_space = []
+        for i in range(3):
+            for j in range(3):
+                if board[i][j] == None:
+                    empty_space.append((i,j))
+        #randomly pick one
+        import random
+        return random.choice(empty_space)
